b053c7cb4ef1646d9f3e366f8f5abae9a0c1e3f3
Faiter119 <olavh96@gmail.com>
2018-10-15 16:13:55 +0200

    basic game functionality

---
+++ NIM.py
@@ -0,0 +1,32 @@
+class NIM:
+
+    def __init__(self, N, K):
+        self.N = N  # Pieces on the board
+        self.K = K  # max pieces you can take, you can take less, but not more.
+
+    def take(self, number):
+
+        if not self.isValidMove(number):
+            return False
+
+        self.N -= number
+        return True
+
+    def isValidMove(self, number):
+
+        return number <= self.K and self.N - number >= 0
+
+    def getValidMoves(self):
+
+        if self.K > self.N - self.K:
+
+            return list(range(1, self.N+1))
+
+        return list(range(1, self.K+1))
+
+    def isDone(self):
+
+        return self.N <= 0
+
+    def __str__(self):
+        return "N="+str(self.N)+", K="+str(self.K)
--- NIMSimulator.py
+++ NIMSimulator.py
@@ -1,4 +1,34 @@
+from NIM import *
+import random
+
 if __name__ == '__main__':
 
 
     print("NIM simulator")
+
+    N = 100
+    K = 40
+
+    game = NIM(N, K)
+
+    print(game.getValidMoves())
+    print(game)
+
+    while not game.isDone():
+
+        player_1 = game.getValidMoves()[random.randint(0, len(game.getValidMoves())-1)]
+
+        game.take(player_1)
+        print("Player 1 took",player_1)
+        print(game)
+        if game.isDone():
+            print("Player 1 won")
+            break
+        player_2 = game.getValidMoves()[random.randint(0, len(game.getValidMoves())-1)]
+        game.take(player_2)
+        print("Player 2 took",player_2)
+        print(game)
+        if game.isDone():
+            print("Player 2 won")
+            break
+
